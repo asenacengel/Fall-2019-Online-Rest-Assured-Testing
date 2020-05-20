4769b0da12bc3c998a2449c55a694dbb95eff5cb
Asena Cengel <asena.eroglu@gmail.com>
2020-05-20 19:02:15 -0400

    api

--- src/test/java/com/automation/tests/day03/ORDSTestDay3.java
+++ src/test/java/com/automation/tests/day03/ORDSTestDay3.java
@@ -22,17 +22,19 @@ public class ORDSTestDay3 {
         baseURI = "http://54.224.118.38:1000/ords/hr";
 
     }
+
     @Test
-    public void verifyFirstRegion(){
-        given().pathParam("id",1).
+    public void verifyFirstRegion() {
+        given().pathParam("id", 1).
                 when().get("/regions/{id}").prettyPeek().
-                then().assertThat().statusCode(200).body("region_name",is("Europe")).
-                body("region_id",is(1)).time(lessThan(5L), TimeUnit.SECONDS);
-                // verify that respoinse time is less than 5 seconds
+                then().assertThat().statusCode(200).body("region_name", is("Europe")).
+                body("region_id", is(1)).time(lessThan(5L), TimeUnit.SECONDS);
+        // verify that respoinse time is less than 5 seconds
 
     }
+
     @Test
-    public void verifyEmployee(){
+    public void verifyEmployee() {
         Response response = given().accept(ContentType.JSON).
                 when().get("/employees");
 
@@ -40,12 +42,10 @@ public class ORDSTestDay3 {
         String nameFirstEmployee = jsonPath.getString("items[0].first_name");// 0 to get first item in the list
         String nameOfLastEmployee = jsonPath.getString("items[-1].first_name"); //-1 to get last item in the list
 
-        System.out.println("first name of 1st employee "+nameFirstEmployee);
-        System.out.println("first name of last employee "+nameOfLastEmployee);
+        System.out.println("first name of 1st employee " + nameFirstEmployee);
+        System.out.println("first name of last employee " + nameOfLastEmployee);
 
-        Map<String,Object> fistEmployee = jsonPath.get("items[0]");
+        Map<String, Object> fistEmployee = jsonPath.get("items[0]");
 
     }
-
-
 }
